389a1445478880ba0bd033b52732137c112d5f12
Matthew Savage <matt@amasses.net>
2012-03-01 12:24:08 +1100

    Added rake file definition for specs, including model files etc

--- lib/fat_zebra.rb
+++ lib/fat_zebra.rb
@@ -1,4 +1,7 @@
-require "FatZebra/version"
+require "fat_zebra/version"
+require 'fat_zebra/models/base'
+require 'fat_zebra/models/purchase'
+require 'fat_zebra/models/refund'
 
 module FatZebra
   # Your code goes here...

4c71e4b33df909e68a7f1a93450243939693dde9
Matthew Savage <matt@amasses.net>
2012-03-02 16:24:55 +1100

    Only assign to attrs if attr present.

--- lib/fat_zebra/models/base.rb
+++ lib/fat_zebra/models/base.rb
@@ -3,7 +3,7 @@ module FatZebra
 		class Base
 			def initialize(attrs = {})
 				attrs.each do |key, val|
-					self.send("#{key}=", val)
+					self.send("#{key}=", val) if self.respond_to?("#{key}=")
 				end
 			end
 		end

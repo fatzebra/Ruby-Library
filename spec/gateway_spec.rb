9866f5f26bc6ebe9516bf19b4e6eaf13c2ef52be
Matthew Savage <matt@amasses.net>
2012-03-02 16:23:59 +1100

    Spec for response and gateway.

---
+++ spec/gateway_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+TEST_USER = "TEST"
+TEST_TOKEN = "TEST"
+TEST_LOCAL = true
+
+describe FatZebra::Gateway do
+	before :each do
+		# Setup the gateway for testing
+		server = TEST_LOCAL == true ? "fatapi.dev" : "gateway.fatzebra.com.au"
+		@gw = FatZebra::Gateway.new(TEST_USER, TEST_TOKEN, server, {:secure => !TEST_LOCAL})
+	end
+
+	it "should require username and token are provided" do
+		lambda { FatZebra::Gateway.new("test", nil) }.should raise_exception(FatZebra::InvalidArgumentError)
+	end
+
+	it "should require that the gateway_server arg is not nil or empty" do
+		lambda { FatZebra::Gateway.new("test", "test", "") }.should raise_exception(FatZebra::InvalidArgumentError)	
+	end
+
+	it "should load a valid instance of the gateway" do
+		@gw.ping.should be_true
+	end
+
+	it "should perform a purchase" do
+		result = @gw.purchase(10000, "Matthew Savage", "5123456789012346", "05/2013", 123, "TEST#{rand}", "1.2.3.4")
+		result.should be_successful
+		result.errors.should be_empty
+	end
+
+	it "should fetch a purchase" do
+		result = @gw.purchase(10000, "Matthew Savage", "5123456789012346", "05/2013", 123, "TES#{rand}T", "1.2.3.4")
+		@gw.purchases(result.purchase.id)
+	end
+end
---
+++ spec/models/response_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe FatZebra::Models::Response do
+	
+end

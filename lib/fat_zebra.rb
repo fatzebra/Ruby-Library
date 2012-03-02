e7d69b1377a191f05b686ddd5e08e2b953e7c680
Matthew Savage <matt@amasses.net>
2012-03-02 16:24:37 +1100

    Requiring of the files.

--- lib/fat_zebra.rb
+++ lib/fat_zebra.rb
@@ -1,5 +1,11 @@
+require 'securerandom'
+require 'json'
+require 'rest_client'
+require 'fat_zebra/errors'
 require "fat_zebra/version"
+require 'fat_zebra/constants'
 require 'fat_zebra/gateway'
 require 'fat_zebra/models/base'
 require 'fat_zebra/models/purchase'
 require 'fat_zebra/models/refund'
+require 'fat_zebra/models/response'

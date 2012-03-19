5e8387ba2c96c4cabfb7b3e0be2a18ac7b25ccd8
Matthew Savage <matt@amasses.net>
2012-03-19 13:32:53 +1100

    Fixed up missing attributes on models

--- lib/fat_zebra/models/purchase.rb
+++ lib/fat_zebra/models/purchase.rb
@@ -1,7 +1,8 @@
 module FatZebra
 	module Models
 		class Purchase < Base
-			attribute :id, :amount, :reference
+			attribute :id, :amount, :reference, :message, :authorization, :transaction_id, :card_number,
+					  :card_holder, :card_expiry, :authorized, :successful
 		end
 	end
 end

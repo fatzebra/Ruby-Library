5b8fb8560055ff68dda31a66c7252fb4c4a85f67
Matthew Savage <matt@amasses.net>
2013-01-18 11:46:12 +1100

    Included currency parameter.

--- lib/fat_zebra/models/purchase.rb
+++ lib/fat_zebra/models/purchase.rb
@@ -2,7 +2,18 @@ module FatZebra
 	module Models
 		class Purchase < Base
 			attribute :id, :amount, :reference, :message, :authorization, :transaction_id, :card_number,
-					  :card_holder, :card_expiry, :authorized, :successful, :card_token, :raw
+					  :card_holder, :card_expiry, :authorized, :successful, :card_token, :raw, :currency
+
+      def approved?
+        self.successful
+      end
+
+      def card_type
+        cards = []
+        cards[4] = "VISA"
+        cards[5] = "MasterCard"
+        cards[3] = "AMEX/JCB"
+      end
 		end
 	end
 end

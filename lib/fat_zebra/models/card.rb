60e84c43c9c9ab421828e8c415ab10931169df4e
Matthew Savage <matt@amasses.net>
2013-01-18 11:53:40 +1100

    Fixed error in merge

--- lib/fat_zebra/models/card.rb
+++ lib/fat_zebra/models/card.rb
@@ -3,30 +3,31 @@ module FatZebra
 		class Card < Base
 			attribute :token, :card_holder, :card_number, :card_expiry, :transaction_count, :raw
 
-    def charge!()
+      def charge!()
 
-    end
-
-    class << self
-      # Tokenizes a credit card
-      # 
-      # @param [String] the credit card holder name
-      # @param [String] the card number
-      # @param [String] the credit card expiry date (mm/yyyy)
-      # @param [String] the card CVV
-      #
-      # @return Response
-      def create(card_holder, card_number, expiry, cvv)
-        params = {
-          :card_holder => card_holder,
-          :card_number => card_number,
-          :card_expiry => expiry,
-          :cvv => cvv
-        }
-
-        response = FatZebra.gateway.make_request(:post, "credit_cards", params)
-        Response.new(response, :card)
       end
-    end
-	end
+
+      class << self
+        # Tokenizes a credit card
+        # 
+        # @param [String] the credit card holder name
+        # @param [String] the card number
+        # @param [String] the credit card expiry date (mm/yyyy)
+        # @param [String] the card CVV
+        #
+        # @return Response
+        def create(card_holder, card_number, expiry, cvv)
+          params = {
+            :card_holder => card_holder,
+            :card_number => card_number,
+            :card_expiry => expiry,
+            :cvv => cvv
+          }
+
+          response = FatZebra.gateway.make_request(:post, "credit_cards", params)
+          Response.new(response, :card)
+        end
+      end
+  	end
+  end
 end
